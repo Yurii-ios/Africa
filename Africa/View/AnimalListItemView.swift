1bd6a3d071e9b10469ba72e3cd5114687e4342ec
Yurii <Yurii>
2020-12-29 17:46:14 +0100

    real data for AnimalListItemView

--- Africa/View/AnimalListItemView.swift
+++ Africa/View/AnimalListItemView.swift
@@ -8,9 +8,12 @@
 import SwiftUI
 
 struct AnimalListItemView: View {
+    //MARK: - PROPERTIES
+    let animal: AnimalModel
+    //MARK: - BODY
     var body: some View {
         HStack(alignment: .center, spacing: 16) {
-            Image("lion")
+            Image(animal.image)
                 .resizable()
                 .scaledToFill()
                 .frame(width: 90, height: 90)
@@ -18,12 +21,12 @@ struct AnimalListItemView: View {
                     RoundedRectangle(cornerRadius: 12.0)
                 )
             VStack(alignment: .leading, spacing: 8) {
-                Text("Lion")
+                Text(animal.name)
                     .font(.title2)
                     .fontWeight(.heavy)
                     .foregroundColor(.accentColor)
                 
-                Text("test,testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest")
+                Text(animal.headline)
                     .font(.footnote)
                     .multilineTextAlignment(.leading)
                     .lineLimit(2)
@@ -33,9 +36,11 @@ struct AnimalListItemView: View {
     }
 }
 
+//MARK: - PREVIEW
 struct AnimalListItemView_Previews: PreviewProvider {
+    static let animals: [AnimalModel] = Bundle.main.decode("animals.json")
     static var previews: some View {
-        AnimalListItemView()
+        AnimalListItemView(animal: animals[1])
             .previewLayout(.sizeThatFits)
             .padding()
     }
