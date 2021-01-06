151888b776922a9bded55954ebe9f96c19975a61
Yurii <Yurii>
2021-01-06 15:15:22 +0100

    add lazyGrid

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -9,11 +9,31 @@ import SwiftUI
 
 struct GalleryView: View {
     //MARK: - PROPERTIES
+    let animals: [AnimalModel] = Bundle.main.decode("animals.json")
+    // SIMPLE GRID DEFINITION
+//    let gridLayout: [GridItem] = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//        GridItem(.flexible())
+//    ]
+    
+    // EFFICIENT GRID DEFINITION
+    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
     
     //MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            //MARK: - GRID
+            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                ForEach(animals) { item in
+                    Image(item.image)
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                    //Text("Gallery")
+                } //: ForEach
+            } //: GRID
         } //: ScrollView
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
