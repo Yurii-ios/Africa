8a6f6be50da5a5eba6216f45af160d90b7132d14
Yurii <Yurii>
2021-01-06 16:03:42 +0100

    add VStack

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct GalleryView: View {
     //MARK: - PROPERTIES
+    @State private var selectedAnimal: String = "lion"
+    
     let animals: [AnimalModel] = Bundle.main.decode("animals.json")
     // SIMPLE GRID DEFINITION
 //    let gridLayout: [GridItem] = [
@@ -23,17 +25,32 @@ struct GalleryView: View {
     //MARK: - BODY
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            //MARK: - GRID
-            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
-                ForEach(animals) { item in
-                    Image(item.image)
-                        .resizable()
-                        .scaledToFit()
-                        .clipShape(Circle())
-                        .overlay(Circle().stroke(Color.white, lineWidth: 1))
-                    //Text("Gallery")
-                } //: ForEach
-            } //: GRID
+            VStack(alignment: .center, spacing: 30) {
+                //MARK: - IMAGE
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                //MARK: - GRID
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                            }
+                        //Text("Gallery")
+                    } //: ForEach
+                } //: GRID
+            } //: VStack
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
+            
         } //: ScrollView
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
